1ec75b32c18874728d3fe817e5e990fc4170275d
Sairo Guanipa <sairojgg@gmail.com>
2021-04-01 16:32:25 -0600

    Built the first design for the chatroom and log in pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require semantic-ui
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown()
+})
